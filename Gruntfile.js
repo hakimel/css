c7409037feabbf825f55f7b088f3b96d82b3b4c6
Hakim El Hattab <hakim.elhattab@gmail.com>
2013-09-18 10:12:02 -0400

    add grunt file, update readme

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		meta: {
+			banner:
+				'/*!\n' +
+				' * http://lab.hakim.se\n' +
+				' *\n' +
+				' * Copyright (C) 2013 Hakim El Hattab, http://hakim.se\n' +
+				' */'
+		},
+
+		cssmin: {
+			compress: {
+				files: {}
+			}
+		},
+
+		sass: {
+			main: {
+				files: {
+					'deviceloop/style.css': 'deviceloop/style.scss'
+				}
+			}
+		},
+
+		jshint: {
+			options: {
+				curly: false,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				eqnull: true,
+				browser: true,
+				expr: true,
+				globals: {
+					head: false,
+					module: false,
+					console: false
+				}
+			},
+			files: [ 'Gruntfile.js', '**/*.js' ]
+		},
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: '.'
+				}
+			}
+		},
+
+		watch: {
+			main: {
+				files: [ 'Gruntfile.js', '*/*.js', '*/*.scss' ],
+				tasks: 'default'
+			},
+		}
+
+	});
+
+	// Dependencies
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+
+	// Default task
+	grunt.registerTask( 'default', [ 'sass' ] );
+
+	// Serve presentation locally
+	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+
+};
